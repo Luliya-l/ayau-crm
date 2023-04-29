bb845c0c3b28debff1d24e09880d24a886b19676
kimboo <kimboo@mail.ru>
2023-04-29 17:12:55 +0600

    chat accept

--- apps/crm-front/components/utils/chat.tsx
+++ apps/crm-front/components/utils/chat.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Form, InputGroup, Nav, Row, Tab } from "react-bootstrap";
 
 const Chat = () => {
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+    const [chatContetnt, setChatContent] = useState('')
 
     return (
         <>
@@ -19,26 +21,100 @@ const Chat = () => {
                     zIndex:'1099', 
                     bottom:'2%',
                     right:'2%'
-                }}>
+                }}
+                onClick={() => handleShow()}
+            >
                 <i className="bi bi-chat-dots py-2" style={{ fontSize: '1.75em', color:'rgba(235, 94, 69, 1)' }} />
             </Button>
-            <Container>
-                <Row>
-                    <Col>
-                    </Col>
-                    <Col>
-                        <Container>
-                            <Row>
-                                <Col>
-                                </Col>
-                            </Row>
-                            <Row>
-                                <Col>
-                                </Col>
-                            </Row>
-                        </Container>
-                    </Col>
-                </Row>
+            <Container 
+                className={`${show ? '' : 'd-none'} position-fixed bg-light p-2 rounded`}
+                style={{bottom:'1%', right:'1%', zIndex:'99999', maxWidth:'390px', height:'70%'}}
+            >
+                <i 
+                    role="button" 
+                    className="bi bi-x text-black fs-3"
+                    onClick={() => handleClose()}
+                ></i>
+                <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+                    <Row>
+                        <Col sm={3}>
+                            <Nav variant="pills" className="flex-column">
+                                <Nav.Item>
+                                <Nav.Link eventKey="first">
+                                    <i className="bi bi-alexa fs-5"></i>
+                                </Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                <Nav.Link eventKey="second">
+                                    <i className="bi bi-people fs-5"></i>
+                                </Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                        </Col>
+                        <Col sm={9}>
+                            <Tab.Content>
+                                <Tab.Pane eventKey="first">
+                                    <Row
+                                        style={{height:'398px'}}
+                                    >
+                                        <Col 
+                                            className=""
+                                             
+                                        >
+                                            <Container>
+                                                {chatContetnt}
+                                            </Container>
+                                        </Col>
+                                    </Row>
+                                    <Row
+                                        style={{height:'5%'}}
+                                    >
+                                        <Col>
+                                            <InputGroup className="mb-3">
+                                                <Form.Control
+                                                    aria-label="Recipient's username"
+                                                    aria-describedby="basic-addon2"
+                                                />
+                                                <Button variant="outline-secondary" id="button-addon2">
+                                                    <i className="bi bi-chat-dots" />
+                                                </Button>
+                                            </InputGroup>
+                                        </Col>
+                                    </Row>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="second">
+                                    <Row
+                                        style={{height:'398px'}}
+                                    >
+                                        <Col 
+                                            className=""
+                                             
+                                        >
+                                            <Container>
+                                                {chatContetnt}
+                                            </Container>
+                                        </Col>
+                                    </Row>
+                                    <Row
+                                        style={{height:'5%'}}
+                                    >
+                                        <Col>
+                                            <InputGroup className="mb-3">
+                                                <Form.Control
+                                                    aria-label="Recipient's username"
+                                                    aria-describedby="basic-addon2"
+                                                />
+                                                <Button variant="outline-secondary" id="button-addon2">
+                                                    <i className="bi bi-chat-dots" />
+                                                </Button>
+                                            </InputGroup>
+                                        </Col>
+                                    </Row>
+                                </Tab.Pane>
+                            </Tab.Content>
+                        </Col>
+                    </Row>
+                </Tab.Container>
             </Container>
         </>
     )
