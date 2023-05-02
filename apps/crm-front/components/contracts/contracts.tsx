c4e205f8f1c4d3ecc357bc9370f9311b2b56cd7a
kimboo <kimboo@mail.ru>
2023-05-03 01:07:34 +0600

    fix

--- apps/crm-front/components/contracts/contracts.tsx
+++ apps/crm-front/components/contracts/contracts.tsx
@@ -8,17 +8,16 @@ import {
 
 import { registerLicense } from "@syncfusion/ej2-base";
 
-import DataSource from '../../data/datasource.json';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectLangState } from 'apps/crm-front/store/langSlice';
-import { useAPI } from 'apps/crm-front/store/apiSlice';
-import { DB } from 'apps/crm-front/specs/custom-types';
+import { DataManager, UrlAdaptor } from '@syncfusion/ej2-data';
+import { AuthState, useAuth } from 'apps/crm-front/store/authSlice';
 
 const Contracts = ({setEditIndex}) => {
+    const auth = useSelector(useAuth) as AuthState;
+
     const localization = useSelector(selectLangState);
     registerLicense('Mgo+DSMBaFt+QHFqVkNrXVNbdV5dVGpAd0N3RGlcdlR1fUUmHVdTRHRcQl5hTn9Tc0RnXXxeeXQ=;Mgo+DSMBPh8sVXJ1S0d+X1RPd11dXmJWd1p/THNYflR1fV9DaUwxOX1dQl9gSX1RcURjXH5adHdXQmA=;ORg4AjUWIQA/Gnt2VFhhQlJBfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hSn5QdEJiWXpfdHBWRWhc;MTQ4NjE2MEAzMjMxMmUzMTJlMzMzNUJnSDVJbXZqODU4NVB5QWV1aXJjMFVFMENtNWhhT2NJdy9ydXMrMFlObmM9;MTQ4NjE2MUAzMjMxMmUzMTJlMzMzNW9HWW1TTkZnOG1aOVR3YndRTmJHZ2xZdVFycjYzWm1FS2pXVkVZSDlDQjg9;NRAiBiAaIQQuGjN/V0d+XU9Hc1RDX3xKf0x/TGpQb19xflBPallYVBYiSV9jS31TdUdkWH9bcXBRQmFeUQ==;MTQ4NjE2M0AzMjMxMmUzMTJlMzMzNWNHSkFzdTU5aUpIdmpsQmxFZC96Z0VKSy8rby9RZFp5elJ3MDR4T0puYzQ9;MTQ4NjE2NEAzMjMxMmUzMTJlMzMzNVZRUERQZEljR1lVKzZoUk5GL1VsUkpKNi9ZUDVFVjBLaC9oQ3kxSDdleDA9;Mgo+DSMBMAY9C3t2VFhhQlJBfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hSn5QdEJiWXpfdHBQR2Bc;MTQ4NjE2NkAzMjMxMmUzMTJlMzMzNUJSWWIrMGhjL0lqclFGNHM5MVl6SE1OSmVwZEpjc3VoTmptU1dnSHZISFE9;MTQ4NjE2N0AzMjMxMmUzMTJlMzMzNVlvcnFjdkY1Q0VVRHF6aWJ3SkFyZlE1WmZldGJHTnNHT1ZwM2FOcmpCQTQ9;MTQ4NjE2OEAzMjMxMmUzMTJlMzMzNWNHSkFzdTU5aUpIdmpsQmxFZC96Z0VKSy8rby9RZFp5elJ3MDR4T0puYzQ9');
-
-    const api = useSelector(useAPI) as DB;
 
     const dispatch = useDispatch();
 
@@ -26,23 +25,30 @@ const Contracts = ({setEditIndex}) => {
         return localization.langs[localization.currentLang]?.params[param];
     }
 
-    const {kanbanData} = DataSource;
-    const data: any  = extend([], kanbanData, null, true);
+    const data = new DataManager({
+        url: 'http://localhost:8000/crm/contracts/get',
+        updateUrl: 'http://localhost:8000/crm/contracts/update',
+        insertUrl: 'http://localhost:8000/crm/contracts/set',
+        removeUrl: 'http://localhost:8000/crm/contracts/delete',
+        adaptor: new UrlAdaptor(),
+        crossDomain: true,
+        headers: [{ Authorization: `Bearer ${auth.authToken}` }]
+    });
     const fields: DialogFieldsModel[] = [
-        { key: 'Id', type: 'TextBox' },
-        { key: 'Status', type: 'DropDown' },
-        { key: 'Estimate', type: 'Numeric' },
-        { key: 'Summary', type: 'TextArea' }
+        { text:'Задача', key: 'name', type: 'TextBox' },
+        { text:'Шаг', key: 'step', type: 'DropDown' },
+        { text:'Бюджет', key: 'budget', type: 'Numeric' },
+        { text:'Задача', key: 'description', type: 'TextArea' }
     ];
 
     return (
         <>
-            <div className="App">
+            <div className="App mt-4">
                 <KanbanComponent 
                     id="kanban" 
-                    keyField="Status" 
+                    keyField="step" 
                     dataSource={data} 
-                    cardSettings={{ contentField: "Summary", headerField: "Id" }} 
+                    cardSettings={{ contentField: "description", headerField: "id", showHeader: false }} 
                     dialogSettings={{ fields: fields }}
                 >
                     <ColumnsDirective>
