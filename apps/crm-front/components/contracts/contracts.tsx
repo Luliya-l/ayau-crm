d880404683df1cad86a1ce599b5a9c5b217e9e9e
kimboo <kimboo@mail.ru>
2023-05-03 10:24:13 +0600

    fix

--- apps/crm-front/components/contracts/contracts.tsx
+++ apps/crm-front/components/contracts/contracts.tsx
@@ -4,6 +4,7 @@ import {
     ColumnDirective, 
     DialogFieldsModel 
 } from "@syncfusion/ej2-react-kanban";
+import { extend, addClass } from '@syncfusion/ej2-base';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { selectLangState } from 'apps/crm-front/store/langSlice';
@@ -44,6 +45,39 @@ const Contracts = () => {
         { text:'Задача', key: 'description', type: 'TextArea' }
     ];
 
+    function cardRendered(args) {
+        const val = args.data.priority;
+        addClass([args.element], val);
+    }
+
+    function columnTemplate(props) {
+        return (<div className="header-template-wrap">
+            <div className={"header-icon e-icons " + props.keyField}></div>
+            <div className="header-text">{props.headerText}</div>
+        </div>);
+    }
+
+    function cardTemplate(props) {
+        return (<div className={"card-template"}>
+            <div className="e-card-header">
+                <div className="e-card-header-caption">
+                    <div className="e-card-header-title e-tooltip-text">{props.name}</div>
+                </div>
+            </div>
+            <div className="e-card-content e-tooltip-text">
+                <div className="e-text">{props.description}</div>
+            </div>
+            <div className="e-card-custom-footer">
+                {props.step.split(",").map((tag) => <div key={tag} className="e-card-tag-field e-tooltip-text">{tag}</div>)}
+                <div className="e-card-avatar">{getString(props.budget)}</div>
+            </div>
+        </div>);
+    }
+
+    function getString(assignee) {
+        return assignee.match(/\b(\w)/g).join("").toUpperCase();
+    }
+
     useEffect(() => {
         kanban.current.refresh();
         dispatch(setLoading(false));
@@ -57,14 +91,36 @@ const Contracts = () => {
                     id="kanban" 
                     keyField="step" 
                     dataSource={data} 
-                    cardSettings={{ contentField: "description", headerField: "id", showHeader: false }} 
+                    cardSettings={{ 
+                        contentField: "description", 
+                        headerField: "id", 
+                        showHeader: false,
+                        template: cardTemplate.bind(this)
+                    }} 
                     dialogSettings={{ fields: fields }}
+                    cardRendered={cardRendered.bind(this)}
                 >
                     <ColumnsDirective>
-                    <ColumnDirective headerText = {`${getParams('primaryContact')}`} keyField="new" />
-                    <ColumnDirective headerText={`${getParams('negotiation')}`} keyField="InProgress" />
-                    <ColumnDirective headerText={`${getParams('makeDecision')}`} keyField="Testing" />
-                    <ColumnDirective headerText={`${getParams('Harmonizationofcontract')}`} keyField="Close" />
+                    <ColumnDirective 
+                        headerText={`${getParams('primaryContact')}`} 
+                        keyField="new" 
+                        template={columnTemplate.bind(this)}
+                    />
+                    <ColumnDirective 
+                        headerText={`${getParams('negotiation')}`} 
+                        keyField="InProgress" 
+                        template={columnTemplate.bind(this)}
+                    />
+                    <ColumnDirective 
+                        headerText={`${getParams('makeDecision')}`} 
+                        keyField="Testing" 
+                        template={columnTemplate.bind(this)}
+                    />
+                    <ColumnDirective 
+                        headerText={`${getParams('Harmonizationofcontract')}`} 
+                        keyField="Close" 
+                        template={columnTemplate.bind(this)}
+                    />
                     </ColumnsDirective>
                 </KanbanComponent>
             </div>
